90f1a58ffba70c48466ea1520eeb8e36c26b778d
unknown <estivensal7@gmail.com>
2018-03-11 17:27:55 -0400

    started prepping files, finished server.js connection

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+### Seeds
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('macNcheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('spicy buffalo', false);
---
+++ server.js
@@ -0,0 +1,32 @@
+//requiring dependencies needed in this file
+const express = require('express');
+const methodOverride = require('method-override');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+//Serve static content for the app from the "public" directory in the application directory
+app.use(express.static('public'));
+
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+//parse application/json
+app.use(bodyParser.json());
+
+//Set Handlebars
+const exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//import routes and give the server access to them
+const routes = require('./controller/burgers_controller.js');
+
+app.use(routes);
+
+app.listen(PORT, function() {
+	console.log('App now listening on localhost: ' + PORT);
+});
